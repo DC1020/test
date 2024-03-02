7ee39665beefaa76366def337362799c6bc42e3c
root <root@X2001926.idsbg.lh.com>
2024-03-02 17:02:09 +0800

    新增Mysql

--- src/test/java/com/foxconn/AppTest.java
+++ src/test/java/com/foxconn/AppTest.java
@@ -1,8 +1,20 @@
 package com.foxconn;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 public class AppTest {
+
+    @Resource
+    public JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void test01() {
+        jdbcTemplate.execute("select * from tbl_user");
+    }
 
 }
