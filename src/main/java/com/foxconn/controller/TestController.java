b5e83510dafad74ae3083c9d52ac81e5c1750ad0
root <root@X2001926.idsbg.lh.com>
2024-03-02 16:36:55 +0800

    third commit

--- src/main/java/com/foxconn/controller/TestController.java
+++ src/main/java/com/foxconn/controller/TestController.java
@@ -1,4 +1,14 @@
 package com.foxconn.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello ssh ....";
+    }
 }
